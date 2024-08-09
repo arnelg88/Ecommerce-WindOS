51974fb2d816371b80dd448b4592344b4a3f8fca
Javier Rodriguez <javierrodriguezb41@outlook.com>
2024-08-08 22:31:35 -0300

    firebase

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7H7253zV_FQe-f7oxJaJL2o4GODhYBxo",
+  authDomain: "dataproducts-4872b.firebaseapp.com",
+  projectId: "dataproducts-4872b",
+  storageBucket: "dataproducts-4872b.appspot.com",
+  messagingSenderId: "441463037675",
+  appId: "1:441463037675:web:082c676b0a9ab5fcb1a8ae"
+};
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
