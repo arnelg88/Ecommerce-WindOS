5d72b82d2a6efdb953e1a66202219d6b575e8dc3
Javier Rodriguez <javierrodriguezb41@outlook.com>
2024-08-26 16:43:30 -0300

    cambios bg

--- src/assets/Components/Elements/NavBar/NavBar.jsx
+++ src/assets/Components/Elements/NavBar/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = ({ setCategory }) => {
         <div className="header">
           <nav className="navbar">
             <Link to="/" className="logoName" onClick={() => handleCategorySelect('')}>
-              <img className="logo" alt="logo" src="https://previews.dropbox.com/p/thumb/ACVJttx3OR9M00BSJn5MAarNl93YK6Xv2l5Pw6p1dGaLO4F8EKiu5xGuYVn-Q3tc-2BHUevdccVabLA5wa1s_e1D_ZqBDVPA2g-CV1RYpVaKwhFJiw8tmWibcdm11wYe6lxja72CLL-Q6fVbWIXDPpDNRvDBSYPXAuCpbFWSro3oRooe8hO0G-0RLhGEEfm4cxQzFgKY70RnrpV70nfti2sT2RV-nL_UhlWggaBN-QTmVgwzA0-biUOT2Y306i3IShSphk5LC-AX77deUd2DlGhXloiaj4_9KwTkClvOGPHQjijeIQm93sXHJShKYTTFD_4zljfJx-MakQeci_aBcKN9/p.png?is_prewarmed=true" />
+              <img className="logo" alt="logo" src="https://previews.dropbox.com/p/thumb/ACaVDbxQFXF59mKdSJdR9zSBGqq1kijwJk94BrZ_fxxHTiz1LG7l6nWeuu30ghfnh87brGyZogFDchgkf4aWdJgHf5zdLq2Kz6Y7z4vCQMPPfjRE_JEuXsfnOBoGL5lbOrcCp2HnQh_SR8aEWj2fkdfUylENXe3IjJfGShaZJKJXmjZMkFUqSMk8LaGH8hvunCA6KxDGN8m5aaMzMhRZ1VCr34XubOX53t-LtoEmZ12mvQWGFlrJspQKppnWAPWbVVBH6moIWhFllmbfJormHHHHr_y6p2M-RAOr9IAakMN8jtqPs7mK_JXNm1YXEK-jxK0/p.png" />
               <h3>WindOS</h3>
             </Link>
             <div className="navbar-center"></div>
